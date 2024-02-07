85ebbe47aa922ce26740549d45a9aaedcb6b0a0b
Mateusz Kardas <kardas-mateusz@wp.pl>
2024-02-07 17:52:53 +0100

    scroll function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,8 @@ export default function Home() {
     const [arrivalDate, setArrivalDate] = useState(dayjs());
     const [departureDate, setDepartureDate] = useState(dayjs().add(1, 'day'));
 
-    const divRef = useRef(null);
+    const scrollDownDiv = useRef(null);
+
 
     useEffect(() => {
         const handleResize = () => {
@@ -36,18 +37,21 @@ export default function Home() {
         config: { tension: 300, friction: 10 }, 
       });
 
+    const scrollDown = () => {
+        scrollDownDiv.current?.scrollIntoView({behavior: 'smooth'});
+    }
 
     return (
         <>
-            <div ref={divRef} style={{ height: windowHeight, backgroundImage: `url(${mainPic})`, backgroundSize: 'cover',  }} 
+            <div  style={{ height: windowHeight, backgroundImage: `url(${mainPic})`, backgroundSize: 'cover',  }} 
                 className="flex justify-center items-center " >
 
                 <h1 className="text-white  text-center font-serif drop-shadow-2xl p-10 w-full text-6xl md:text-8xl" style={{backdropFilter: "brightness(60%)"}} >Cornelia Street Hotel</h1>
 
-                <animated.div style={arrowAnimation} className="absolute bottom-0 flex justify-center">
-                    <div className="w-1 h-8 absolute  bottom-0 mr-5 border-solid border-2 border-gray-400 -rotate-45"></div>
-                    <div className="w-1 h-12 absolute bottom-1  border-solid border-2 border-gray-400"></div>
-                    <div className="w-1 h-8 absolute  bottom-0 ml-5 border-solid border-2 border-gray-400 rotate-45"></div>
+                <animated.div style={arrowAnimation} className="absolute bottom-0 flex justify-center cursor-pointer" onClick={scrollDown}>
+                    <div className="w-1 h-8 absolute  bottom-0 mr-5 border-solid border-2 border-gray-500 -rotate-45"></div>
+                    <div className="w-1 h-12 absolute bottom-1  border-solid border-2 border-gray-500"></div>
+                    <div className="w-1 h-8 absolute  bottom-0 ml-5 border-solid border-2 border-gray-500 rotate-45"></div>
                 </animated.div>
                 
 
@@ -56,7 +60,7 @@ export default function Home() {
 
                 <h1 className="text-xl">Zaplanuj swój pobyt razem z nami</h1>
 
-                <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
+                <div ref={scrollDownDiv} className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0" >
                     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
 
                         <DatePicker 
