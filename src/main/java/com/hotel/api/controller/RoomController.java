213707bb0ea79407cf85cf8d060bdce992bf3ac3
Mateusz Kardas <kardas-mateusz@wp.pl>
2024-02-15 21:48:53 +0100

    date exception handlers

--- src/main/java/com/hotel/api/controller/RoomController.java
+++ src/main/java/com/hotel/api/controller/RoomController.java
@@ -2,16 +2,18 @@ package com.hotel.api.controller;
 
 import com.hotel.api.dto.NewRoomDTO;
 import com.hotel.api.dto.RoomDTO;
-import com.hotel.api.exception.RoomExceptionHandler;
+import com.hotel.api.exception.BookRoomDateException;
 import com.hotel.api.model.Room;
 import com.hotel.api.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,7 +51,24 @@ public class RoomController {
     @ResponseBody
     @CrossOrigin
     public ResponseEntity<?> findAvailableRooms(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
-        List<RoomDTO> availableRooms = mapToPostDTO(roomService.getAvailableRooms(LocalDate.parse(startDate), LocalDate.parse(endDate)));
+
+        LocalDate startDateLocal;
+        LocalDate endDateLocal;
+
+        try {
+            startDateLocal = LocalDate.parse(startDate);
+            endDateLocal = LocalDate.parse(endDate);
+        } catch (Exception e) {
+            throw new BookRoomDateException("Wprowadzono nieporawny format daty");
+        }
+
+        if(startDateLocal.isAfter(endDateLocal))
+            throw new BookRoomDateException("Data przyjadu musi być wcześniejsza niż data wyjazdu");
+
+        if(startDateLocal.isBefore(LocalDate.now()))
+            throw new BookRoomDateException("Nie można wyszukać ofert z przeszłości");
+
+        List<RoomDTO> availableRooms = mapToPostDTO(roomService.getAvailableRooms(startDateLocal, endDateLocal));
         return new ResponseEntity<>(availableRooms, HttpStatus.OK);
     }
 
--- src/main/java/com/hotel/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hotel/api/exception/GlobalExceptionHandler.java
@@ -1,11 +1,37 @@
 package com.hotel.api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler()
+    @ExceptionHandler(BookRoomDateException.class)
+    public ResponseEntity<ErrorObject> handleBookRoomDateException(BookRoomDateException ex){
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
+
+        return new ResponseEntity<>(errorObject, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorObject> handleMissingParameters(){
+
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage("Nie wprowadzono wszystkich wymaganych danych");
+        errorObject.setTimestamp(new Date());
+
+        return new ResponseEntity<>(errorObject, HttpStatus.BAD_REQUEST);
+    }
 
 }
