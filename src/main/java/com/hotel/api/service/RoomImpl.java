2a9981de13cd2a0f4039cb55ecea068cfd88fd53
Mateusz Kardas <kardas-mateusz@wp.pl>
2024-02-18 16:33:12 +0100

    reserve room with exceptions

--- src/main/java/com/hotel/api/service/RoomImpl.java
+++ src/main/java/com/hotel/api/service/RoomImpl.java
@@ -1,7 +1,6 @@
 package com.hotel.api.service;
 
 
-import com.hotel.api.dto.RoomDTO;
 import com.hotel.api.model.Room;
 import com.hotel.api.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomImpl implements RoomService {
@@ -26,6 +26,11 @@ public class RoomImpl implements RoomService {
     @Override
     public List<Room> getAvailableRooms(LocalDate dateStart, LocalDate dateEnd) {
         return roomRepository.getAvailableRooms(dateStart, dateEnd);
+    }
+
+    @Override
+    public Optional<Room> getRoomByID(Integer roomID) {
+        return roomRepository.findById(roomID);
     }
 
     @Override
