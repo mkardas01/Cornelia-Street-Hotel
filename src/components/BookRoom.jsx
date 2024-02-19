561dc6ba2f3dca2810d871313c70759f698f1f7b
= <=>
2024-02-19 18:06:10 +0100

    booking details and notifications

--- src/components/BookRoom.jsx
+++ src/components/BookRoom.jsx
@@ -1,19 +1,20 @@
 import mainPic from "/assets/bookRoom.jpg";
 import {TextField, Checkbox, FormControlLabel, FormHelperText } from "@mui/material";
 import {useState} from "react";
-
+import {NavLink} from 'react-router-dom';
 import SendIcon from "@mui/icons-material/Send.js";
 import LoadingButton from "@mui/lab/LoadingButton";
 
 import {useLocation, useParams} from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 import axios from 'axios';
 
 import dayjs from "dayjs";
 import "dayjs/locale/pl";
+import NotificationBar from "./NotificationBar.jsx";
 
 export default function BookRoom( props ) {
 
@@ -41,30 +42,14 @@ export default function BookRoom( props ) {
     const [phoneError, setPhoneError] = useState('');
     const [termsError, setTermsError] = useState('');
 
+    const [notificationMessage, setNotificationMessage] = useState("");
+    const [notificationType, setNotificationType] = useState("error");
+    const [openNotificationBar, setOpenNotificationBar] = useState(false);
 
 
-    const valid = () => {
-        let reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const [bookingResults, setBookingResults] = useState(false);
+    const [bookingResultsData, setBookingResultsData] = useState();
 
-        if(name.length === 0 )
-            setNameError("Imię nie może pozostać puste");
-
-        if(surname.length === 0)
-            setSurnameError("Nazwisko nie może pozostać puste")
-
-        if(email.length === 0)
-            setEmailError("Email nie może pozostać pusty")
-        else if(!reEmail.test(email))
-            setEmailError("Podano nie poprawny adres email")
-
-        if(phone.length === 0)
-            setPhoneError("Numer telefonu nie może pozostać pusty")
-        else if(phone.length !== 9)
-            setPhoneError("Podano nie poprawny numer telefonu")
-
-        return !(nameError.length>0 || surnameError.length>0 || emailError.length>0 || phoneError.length>0);
-
-    }
 
     const handlePhoneNumber = (e) => {
         const regex = /^[0-9\b]+$/;
@@ -93,11 +78,53 @@ export default function BookRoom( props ) {
                     }
                 });
 
+            setBookingResultsData(response.data);
+            setBookingResults(true);
+            setNotificationType("success");
+            setNotificationMessage("Pokój został zarezerwowany");
+            setOpenNotificationBar(true);
+
         } catch (error) {
+            setNotificationType("error");
+            setOpenNotificationBar(true);
+            setNotificationMessage(error?.response?.data?.message ? error.response.data.message : "Przepraszamy wystąpił błąd w trakcie komunikacji z serwerem");
 
         } finally {
             setLoading(false);
+
         }
+    }
+
+    const valid = () => {
+        let reEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        let nameErrorText = "";
+        let surnameErrorText = "";
+        let emailErrorText = "";
+        let phoneErrorText = "";
+
+        if(name.length === 0)
+            nameErrorText = "Imię nie może pozostać puste";
+
+        if(surname.length === 0)
+            surnameErrorText = "Nazwisko nie może pozostać puste";
+
+        if(email.length === 0)
+            emailErrorText = "Email nie może pozostać pusty";
+        else if(!reEmail.test(email))
+            emailErrorText = "Podano niepoprawny adres email";
+
+        if(phone.length === 0)
+            phoneErrorText = "Numer telefonu nie może pozostać pusty";
+        else if(phone.length !== 9 || phone[0] === '0')
+            phoneErrorText = "Podano niepoprawny numer telefonu";
+
+        setNameError(nameErrorText);
+        setSurnameError(surnameErrorText);
+        setEmailError(emailErrorText);
+        setPhoneError(phoneErrorText);
+
+        return !(nameErrorText || surnameErrorText || emailErrorText || phoneErrorText);
     }
 
 
@@ -110,79 +137,92 @@ export default function BookRoom( props ) {
 
     return (
         <>
+            <NotificationBar type={notificationType} notificationMessage={notificationMessage}
+                             open={openNotificationBar} setOpen={setOpenNotificationBar}/>
+
+
             <div style={{backgroundImage: `url(${mainPic})`}}
                  className="flex flex-col justify-center items-center bg-cover bg-center w-full h-screen">
+                {!bookingResults &&
+                    <div
+                        className="flex flex-col justify-center items-center my-32 space-y-5 w-screen h-fit md:flex-row md:space-x-5 md:space-y-0">
+                        <div
+                            className="flex flex-col bg-white w-3/4 rounded-3xl p-10 md:w-1/2 md:space-y-8 md:min-h-full">
+                            <h1 className="font-serif drop-shadow-2xl text-xl mb-5 md:text-2xl">Dane rezerwującego</h1>
 
-                <div className="flex flex-col justify-center items-center my-32 space-y-5 w-screen h-fit md:flex-row md:space-x-5 md:space-y-0">
+                            <form noValidate autoComplete="off" className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                                <TextField
+                                    id="fName"
+                                    onChange={(e) => {
+                                        setName(e.target.value);
+                                        setNameError('')
+                                    }}
+                                    label="Imię"
+                                    variant="outlined"
+                                    required
+                                    error={nameError.length > 0}
+                                    helperText={nameError}
+                                />
 
-                    <div className="flex flex-col bg-white w-3/4 rounded-3xl p-10 md:w-1/2 md:space-y-8 md:min-h-full">
-                        <h1 className="font-serif drop-shadow-2xl text-xl mb-5 md:text-2xl">Dane rezerwującego</h1>
-
-                        <form noValidate autoComplete="off" className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                            <TextField
-                                id="fName"
-                                onChange={(e) => {setName(e.target.value); setNameError('')}}
-                                label="Imię"
-                                variant="outlined"
-                                required
-                                error={nameError.length > 0}
-                                helperText={nameError}
-                            />
-
-                            <TextField
-                                id="outlined-basic"
-                                onChange={(e) => { setSurname(e.target.value); setSurnameError('')}}
-                                label="Nazwisko"
-                                variant="outlined"
-                                required
-                                error={surnameError.length > 0}
-                                helperText={surnameError}
-                            />
-
-
-                            <TextField
-                                id="outlined-basic"
-                                onChange={(e) => {setEmail(e.target.value); setEmailError('')}}
-                                label="Email"
-                                variant="outlined"
-                                required
-                                error={emailError.length > 0}
-                                helperText={emailError}
-                            />
-
-                            <TextField
-                                id="outlined-basic"
-                                onChange={(e) => handlePhoneNumber(e)}
-                                value={phone}
-                                label="Telefon"
-                                variant="outlined"
-                                required
-                                maxLength="9"
-                                error={phoneError.length > 0}
-                                helperText={phoneError}
-                            />
-
-                            <FormControlLabel
-                                required
-                                label="Akceptuje regulamin pobytu"
-                                control={<Checkbox />}
-                                labelPlacement="start"
-                                className="justify-self-start"
-
-                            />
+                                <TextField
+                                    id="outlined-basic"
+                                    onChange={(e) => {
+                                        setSurname(e.target.value);
+                                        setSurnameError('')
+                                    }}
+                                    label="Nazwisko"
+                                    variant="outlined"
+                                    required
+                                    error={surnameError.length > 0}
+                                    helperText={surnameError}
+                                />
 
 
+                                <TextField
+                                    id="outlined-basic"
+                                    onChange={(e) => {
+                                        setEmail(e.target.value);
+                                        setEmailError('')
+                                    }}
+                                    label="Email"
+                                    variant="outlined"
+                                    required
+                                    error={emailError.length > 0}
+                                    helperText={emailError}
+                                />
 
-                        </form>
-                    </div>
+                                <TextField
+                                    id="outlined-basic"
+                                    onChange={(e) => handlePhoneNumber(e)}
+                                    value={phone}
+                                    label="Telefon"
+                                    variant="outlined"
+                                    required
+                                    maxLength="9"
+                                    error={phoneError.length > 0}
+                                    helperText={phoneError}
+                                />
 
-                    <div className="flex flex-col bg-white w-3/4 rounded-3xl p-10 space-y-5 md:w-1/4 md:h-full">
+                                <FormControlLabel
+                                    required
+                                    label="Akceptuje regulamin pobytu"
+                                    control={<Checkbox/>}
+                                    labelPlacement="start"
+                                    className="justify-self-start"
 
-                        <div>
-                            <p className="font-semibold flex justify-between">
-                                <span>{startDate.format("DD MMM YYYY")}</span>
-                                <span><FontAwesomeIcon icon={faArrowRight}/></span>
-                                <span>{endDate.format("DD MMM YYYY")}</span>
+                                />
+
+
+                            </form>
+                        </div>
+
+                        <div className="flex flex-col bg-white w-3/4 rounded-3xl p-10 space-y-5 md:w-1/4 md:h-full">
+
+                            <div>
+                                <p className="font-semibold flex justify-between">
+                                    <span>{startDate.format("DD MMM YYYY")}</span>
+                                    <span><FontAwesomeIcon icon={faArrowRight}/></span>
+                                    <span>{endDate.format("DD MMM YYYY")}</span>
                                 </p>
                             </div>
 
@@ -207,7 +247,7 @@ export default function BookRoom( props ) {
 
 
                             <LoadingButton
-                                endIcon={<SendIcon />}
+                                endIcon={<SendIcon/>}
                                 onClick={reserveRoom}
                                 loading={loading}
                                 loadingPosition="end"
@@ -220,11 +260,67 @@ export default function BookRoom( props ) {
 
                             </LoadingButton>
 
+                        </div>
+                    </div>
+                }
+                {bookingResults &&
+                    <div
+                        className="flex flex-col justify-center items-center my-32 space-y-5 w-screen h-fit md:flex-row md:space-x-5 md:space-y-0">
+
+                        <div
+                            className="flex flex-col bg-white w-3/4 space-y-2 rounded-3xl p-10 md:w-1/2  min-h-full">
+
+                            <h1 className="font-serif drop-shadow-2xl text-xl mb-5 md:text-2xl">Twoja rezerwacja została
+                                potwierdzona</h1>
+
+                            <div className="pb-5">
+                                <p className="font-semibold flex justify-between mb-5">
+                                    <span>{startDate.format("DD MMM YYYY")}</span>
+                                    <span><FontAwesomeIcon icon={faArrowRight}/></span>
+                                    <span>{endDate.format("DD MMM YYYY")}</span>
+                                </p>
+
+                                <p className="font-medium text-gray-500">Pokój: <span
+                                    className="text-black font-medium">{room.name}</span></p>
+                                <p className="font-medium text-gray-500">Numer pokoju: <span
+                                    className="text-black font-medium">{room.number}</span></p>
+                                <p className="font-medium text-gray-500">Piętro: <span
+                                    className="text-black font-medium">{room.floorNumber}</span></p>
+                            </div>
+
+
+                            <p className="font-medium text-gray-500">Imię: <span
+                                className="text-black font-medium">{bookingResultsData.name}</span></p>
+
+                            <p className="font-medium text-gray-500">Naziwsko: <span
+                                className="text-black font-medium">{bookingResultsData.surname}</span></p>
+
+                            <p className="font-medium text-gray-500">E-mail: <span
+                                className="text-black font-medium">{bookingResultsData.email}</span></p>
+
+                            <p className="font-medium text-gray-500">Numer rezerwacji: <span
+                                className="text-black font-medium">{bookingResultsData.reservationNumber}</span></p>
+
+                            <p className="font-medium text-gray-500">Telefon: <span
+                                className="text-black font-medium">{bookingResultsData.phone}</span></p>
+
+                            <h2 className="pt-10 text-sm font-medium text-gray-500">W razie niezgodności podanych
+                                powyżej danych prosimy o kontakt</h2>
+                        </div>
+
                     </div>
 
-                </div>
-            </div>
+                }
 
+            </div>
+            <NavLink to={{pathname: '/'}} >
+                <div style={{backgroundColor: '#2d2d33'}} className="flex flex-col justify-center items-center rounded-full w-12 h-12
+                                                                     p-10 fixed bottom-2 right-2 z-10 hover:cursor-pointer">
+                    <span className="space-y-2 text-5xl" >
+                        <FontAwesomeIcon icon={faArrowLeft} style={{color: "#ffffff"}}/>
+                    </span>
+                </div>
+            </NavLink>
 
         </>
 
