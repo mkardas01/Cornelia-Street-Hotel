6f8d26d946142188818140113e07511aa1e36304
= <=>
2024-02-20 20:30:05 +0100

    navbar login link, animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useTransition,useSpring, animated} from "@react-spring/web";
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
 
 
 AnimatedSpan.propTypes = {
@@ -35,7 +36,14 @@ export default function NavBar() {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 820);
     const [navBarColor, setNavBarColor] = useState(window.scrollY > window.innerHeight ? "#2d2d33" : "transparent");
 
-    const options = ['O nas', 'Twoje rezerwacje', 'Zaloguj się']
+    const options = [
+        { name: 'O nas', link: '/login' },
+        { name: 'Twoje rezerwacje', link: '/login' },
+        { name: 'Zaloguj się', link: '/login' }
+    ];
+
+
+
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
@@ -51,21 +59,21 @@ export default function NavBar() {
     const AnimatedNavbarColor = useSpring({
         backgroundColor: navBarColor,
         config: {duration: 400},
-        });
+    });
 
-    
+
     useEffect(() => {
         const handleResizeY = () => {
             let color = window.scrollY >= window.innerHeight-100 ? "#2d2d33" : "transparent";
             setNavBarColor(color);
 
         };
-    
+
         window.addEventListener("scroll", handleResizeY);
         return () => window.removeEventListener("scroll", handleResizeY);
-    
+
     }, []);
-    
+
 
     useEffect(() => {
         const handleResize = () => {
@@ -79,12 +87,12 @@ export default function NavBar() {
         };
     }, []);
 
-   
+
 
     return (
         <>
             {isMobile && !isOpen && (
-                <div onClick={toggleNavbar} style={{ backgroundColor: '#2d2d33' }} className="flex flex-col justify-center items-center rounded-full w-12 h-12 
+                <div onClick={toggleNavbar} style={{ backgroundColor: '#2d2d33' }} className="flex flex-col justify-center items-center rounded-full w-12 h-12
                                                                                                     p-10 fixed top-2 left-2 z-10 hover:cursor-pointer">
                     <span className="space-y-2">
                         <div className="border-t-2 w-7 border-white"></div>
@@ -93,34 +101,34 @@ export default function NavBar() {
                     </span>
                     <span className="text-sm text-white mt-2 tracking-widest">MENU</span>
                 </div>
-            )} 
-            
+            )}
+
             {isMobile && transition(
-                    (styles, item) => item && 
-                        <animated.div style={{ ...styles, backgroundColor: '#2d2d33' }} className="text-white h-52 fixed top-0 w-full z-10">
-                            <div className="mx-8 space-y-4 p-4 pb-8 md:hidden">
-                                <span className="flex justify-start text-2xl hover:cursor-pointer w-fit " onClick={toggleNavbar}> x </span>
-                                <div className="flex flex-col justify-center items-center space-y-4 font-bold">
+                (styles, item) => item &&
+                    <animated.div style={{ ...styles, backgroundColor: '#2d2d33' }} className="text-white h-52 fixed top-0 w-full z-10">
+                        <div className="mx-8 space-y-4 p-4 pb-8 md:hidden">
+                            <span className="flex justify-start text-2xl hover:cursor-pointer w-fit " onClick={toggleNavbar}> x </span>
+                            <div className="flex flex-col justify-center items-center space-y-4 font-bold">
                                 {options.map((option, index) => (
                                     <AnimatedSpan key={index} action={toggleNavbar}>
-                                        {option}
+                                        {option.name}
                                     </AnimatedSpan>
                                 ))}
-                                </div>
                             </div>
-                        </animated.div> 
-                    
+                        </div>
+                    </animated.div>
+
             )}
 
             {!isMobile && (
                 <animated.div style={{...AnimatedNavbarColor}} className="text-white h-20 fixed top-0 w-full z-10">
-                        <div className="h-full flex justify-end items-center space-x-6 font-bold mx-8">
-                            {options.map((option, index) => (
-                                <AnimatedSpan key={index}>
-                                    {option}
-                                </AnimatedSpan>
-                            ))}
-                        </div>
+                    <div className="h-full flex justify-end items-center space-x-6 font-bold mx-8">
+                        {options.map((option, index) => (
+                            <AnimatedSpan key={index}>
+                                <Link to={{pathname: option.link}}> {option.name} </Link>
+                            </AnimatedSpan>
+                        ))}
+                    </div>
                 </animated.div>
             )}
 
