fb849a0824c8a42aadc290d2a392ac2bee9ababf
= <=>
2024-02-21 23:01:05 +0100

    user class

--- src/main/java/com/hotel/api/model/Reservation.java
+++ src/main/java/com/hotel/api/model/Reservation.java
@@ -7,8 +7,7 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ src/main/java/com/hotel/api/user/Role.java
@@ -0,0 +1,4 @@
+package com.hotel.api.user;
+
+public enum Role {
+}
