88e7a0e24f10cb8b5865206515db6e393818f943
= <=>
2024-02-24 01:19:17 +0100

    hidde navbar when login page

--- src/components/BackHome.jsx
+++ src/components/BackHome.jsx
@@ -7,7 +7,7 @@ export function BackHome() {
         <>
             <Link to={{pathname: '/'}} >
                 <div style={{backgroundColor: '#2d2d33'}} className="flex flex-col justify-center items-center rounded-full w-12 h-12
-                                                                     p-10 fixed bottom-2 right-2 z-10 hover:cursor-pointer">
+                                                                     p-10 fixed bottom-2 left-2 z-10 hover:cursor-pointer">
                     <span className="space-y-2 text-5xl" >
                         <FontAwesomeIcon icon={faArrowLeft} style={{color: "#ffffff"}}/>
                     </span>
