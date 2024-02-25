9d76fa36e7961539442f3061b379b6231954a560
Mateusz Kardas <kardas-mateusz@wp.pl>
2024-02-25 22:11:44 +0100

    protected route

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -12,17 +12,61 @@ import NavBar from './components/NavBar'
 import Home from './components/Home'
 import BookRoom from "./components/BookRoom.jsx";
 import Login_Register from "./components/Login_Register.jsx";
+import {BackHome} from "./components/BackHome.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <Router>
-          <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/bookRoom/:id" element={<BookRoom />} />
-          <Route path="/login" element={<Login_Register type="login" />} />
-          <Route path="/register" element={<Login_Register type="register" />} />
-        </Routes>
-      </Router>
-    </React.StrictMode>,
-)
+import Cookies from 'js-cookie';
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Logout from "./components/Logout.jsx";
+
+let root = ReactDOM.createRoot(document.getElementById('root'));
+
+export default function App(){
+
+    const [isLoggedIn, setIsLoggedIn] = useState();
+
+    useEffect(() => {
+        const token = Cookies.get('token');
+
+        if (token) {
+            console.log('Token exists:', token);
+            setIsLoggedIn(true);
+
+        } else {
+            console.log('Token does not exist');
+            setIsLoggedIn(false);
+        }
+
+
+    }, []);
+
+    return(
+        <React.StrictMode>
+            <Router>
+                <NavBar isLoggedIn={isLoggedIn} />
+                <Routes>
+
+                    <Route path="/" element={<Home />} />
+                    <Route path="/bookRoom/:id" element={<> <BookRoom /> <BackHome /> </>} />
+
+                    <Route element={<ProtectedRoute isLoggedIn={!isLoggedIn} /> } >
+                        <Route path="/login" element={ <> <Login_Register type="login" /> <BackHome /> </> } />
+                        <Route path="/register" element={<> <Login_Register type="register" /> <BackHome /> </> } />
+                    </Route>
+
+
+                    <Route element={<ProtectedRoute isLoggedIn={isLoggedIn} /> } >
+                        <Route path="/logout" element={ <Logout/>} />
+                    </Route>
+
+
+
+                    <Route path="/*" element={<Navigate to="/" />} />
+
+                </Routes>
+            </Router>
+        </React.StrictMode>
+    )
+}
+
+
+root.render(<App />);
