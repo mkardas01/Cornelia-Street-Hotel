64b86c51b2b2acbe688fea83cd34234f61d43598
= <=>
2024-02-26 10:51:59 +0100

    Create MainPicWithArrow component, use it in home and reservation

--- src/main.jsx
+++ src/main.jsx
@@ -17,10 +17,13 @@ import {BackHome} from "./components/BackHome.jsx";
 import Cookies from 'js-cookie';
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Logout from "./components/Logout.jsx";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
+import UserReservation from "./components/UserReservation.jsx";
 
 let root = ReactDOM.createRoot(document.getElementById('root'));
 
-export default function App(){
+export default function App() {
 
     const [isLoggedIn, setIsLoggedIn] = useState();
 
@@ -39,28 +42,28 @@ export default function App(){
 
     }, []);
 
-    return(
+    return (
         <React.StrictMode>
             <Router>
-                <NavBar isLoggedIn={isLoggedIn} />
+                <NavBar isLoggedIn={isLoggedIn}/>
                 <Routes>
 
-                    <Route path="/" element={<Home />} />
-                    <Route path="/bookRoom/:id" element={<> <BookRoom /> <BackHome /> </>} />
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/bookRoom/:id" element={<> <BookRoom/> <BackHome/> </>}/>
 
-                    <Route element={<ProtectedRoute isLoggedIn={!isLoggedIn} /> } >
-                        <Route path="/login" element={ <> <Login_Register type="login" /> <BackHome /> </> } />
-                        <Route path="/register" element={<> <Login_Register type="register" /> <BackHome /> </> } />
+                    <Route element={<ProtectedRoute isLoggedIn={!isLoggedIn}/>}>
+                        <Route path="/login" element={<> <Login_Register type="login"/> <BackHome/> </>}/>
+                        <Route path="/register" element={<> <Login_Register type="register"/> <BackHome/> </>}/>
                     </Route>
 
 
-                    <Route element={<ProtectedRoute isLoggedIn={isLoggedIn} /> } >
-                        <Route path="/logout" element={ <Logout/>} />
+                    <Route element={<ProtectedRoute isLoggedIn={isLoggedIn}/>}>
+                        <Route path="/logout" element={<Logout/>}/>
+                        <Route path="/reservation" element={<> <UserReservation /> <BackHome/> </>} />
                     </Route>
 
 
-
-                    <Route path="/*" element={<Navigate to="/" />} />
+                    <Route path="/*" element={<Navigate to="/"/>}/>
 
                 </Routes>
             </Router>
@@ -69,4 +72,8 @@ export default function App(){
 }
 
 
-root.render(<App />);
+root.render(<DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+>
+    <App/>
+</DevSupport>);
