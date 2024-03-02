835ee659d76dce91d1dcf9b380c65981764a627c
Mateusz Kardas <kardas-mateusz@wp.pl>
2024-03-02 23:27:29 +0100

    add dialog window

---
+++ src/components/admin/DialogWindow.jsx
@@ -0,0 +1,50 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    useMediaQuery,
+    useTheme
+} from "@mui/material";
+import ButtonLabels from "./ButtonLabels.jsx";
+
+export default function DialogWindow({open, setOpen}) {
+
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('lg'));
+
+    const handleClose = () => {
+        setOpen({status: false, action: null});
+    };
+
+    return (
+        <>
+
+            <Dialog
+                open={open.status}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+                className=""
+            >
+                <DialogTitle id="responsive-dialog-title" >
+                    {ButtonLabels[open.action]?.title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {ButtonLabels[open.action]?.message}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleClose}>
+                        Odrzuć
+                    </Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Potwierdź
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
