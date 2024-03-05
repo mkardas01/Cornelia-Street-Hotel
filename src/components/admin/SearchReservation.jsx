a2717df53d3cdaa45b52916325c491657bcbb20e
Mateusz Kardas <kardas-mateusz@wp.pl>
2024-03-05 11:40:49 +0100

    props passing admin panel

--- src/components/admin/SearchReservation.jsx
+++ src/components/admin/SearchReservation.jsx
@@ -13,7 +13,7 @@ import MenageButtons from "./MenageButtons.jsx";
 import DialogWindow from "./DialogWindow.jsx";
 
 
-export default function SearchReservation({setType, setNotificationMessage, setNavBarOpen}) {
+export default function SearchReservation(props) {
 
     const BASE_URL = "http://localhost:8080/api";
 
@@ -55,18 +55,18 @@ export default function SearchReservation({setType, setNotificationMessage, setN
             setReservations(response.data);
             setSearched(true);
 
-            setType("success");
-            setNotificationMessage(`Znaleziona ilośc rezerwacji: ${response.data.length}`)
-            setNavBarOpen(true);
+            props.setType("success");
+            props.setNotificationMessage(`Znaleziona ilośc rezerwacji: ${response.data.length}`)
+            props.setNavBarOpen(true);
 
             setTimeout(() => {
                 scrollDown();
             }, 100);
 
         }catch(error){
-            setType("error");
-            setNotificationMessage(error?.response?.data?.message ? error.response.data.message : "Przepraszamy wystąpił błąd w trakcie komunikacji z serwerem");
-            setNavBarOpen(true);
+            props.setType("error");
+            props.setNotificationMessage(error?.response?.data?.message ? error.response.data.message : "Przepraszamy wystąpił błąd w trakcie komunikacji z serwerem");
+            props.setNavBarOpen(true);
         }
     }
 
@@ -74,8 +74,7 @@ export default function SearchReservation({setType, setNotificationMessage, setN
     return(
         <>
             <DialogWindow open={open} setOpen={setOpen} reservations={reservations} setReservations={setReservations}
-                          setType={setType} setNotificationMessage={setNotificationMessage}
-                          setNavBarOpen={setNavBarOpen} />
+                          {...props} />
 
             <div className="flex flex-col items-center justify-center max-w-7xl ">
                 <div className="flex justify-center items-center  h-screen">
